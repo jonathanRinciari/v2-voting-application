9d8e28da149bf5f7e55ee83f152da9da3f9a554b
jon <jrinciari@or-specialties.com>
2017-12-09 19:27:00 +0000

    Set up one vote per user, based on username. Allowed custom votes and at time of adding new vote it will vote & add to options array for others to use

--- routes/poll.js
+++ routes/poll.js
@@ -15,15 +15,9 @@ router.post('/create', function(req, res){
     var options = req.body.options.split(',')
     var optionsArr = [];
     for(var i = 0; i < options.length; i++){
-        // optionObj[options[i]] = {votes: 0, name: options[i]
         optionsArr.push({title: options[i]})
     }
-    
- 
-    // var optionObj = {};
-    // for(var i = 0; i < options.length; i++){
-    //     optionObj[options[i]] = 0;
-    // }
+
     var newPoll = ({
         author: author,
         title: title,
@@ -35,9 +29,7 @@ router.post('/create', function(req, res){
     
     Poll.create(newPoll, function(err, newlyCreated){
         if(err) throw err;
-        
-        // 
-        res.redirect(`/${newlyCreated._id}`)
+        res.redirect('/')
     })
 })
 
@@ -45,7 +37,7 @@ router.post('/create', function(req, res){
 router.get('/:id', function(req, res){
     Poll.findById(req.params.id, function(err, foundPoll){
         if(err) throw err;
-        res.render('poll', {foundPoll: foundPoll})
+        res.render('poll', {foundPoll: foundPoll.options})
     })
 })
 
@@ -53,35 +45,66 @@ router.put('/:id', function(req, res){
     var id = req.params.id;
     var userVote = req.body.vote
     var user = req.user.username
-    Poll.findById(id, function(err, data){
-        if(err) throw err;
-        submitAnswer(userVote, res, id, user)
-    })
-    
+     checkVoted(id, user)
+        .then(function(newVoter){
+          if(newVoter){
+              console.log(newVoter)
+             submitAnswer(userVote, res, id, user)
+           } else {
+               res.json({message: 'Already voted!'})
+           }
+        })
+           .catch(function(err){
+               console.log(err)
+           })
 })
+
 
 router.post('/:id', function(req, res){
 var id = req.params.id
 var customVote = req.body.vote
 var user = req.user.username
+    checkVoted(id, user)
+      .then(function(newUser){
+   if(newUser){
     Poll.findByIdAndUpdate(id, { $push: {options: {title: customVote}}}, {new: true}, 
     function(err, data){
         if(err) throw err;
         submitAnswer(customVote, res, id, user)
     })
+   } else {
+       res.json({message: 'You already voted'})
+   }
+} );
 
 })
 
 function submitAnswer(field, res, id, user){
   //  $push: {'voters': user}}, 
+  
     Poll.findOneAndUpdate(
     { options: {$elemMatch: {title: field}}},
-    { $inc: { 'options.$.vote': 1}, $push: {'voters': user}},
- 
+    { $inc: { 'options.$.vote': 1}, $push: {voters: user}},
     function(err, poll){
         if(err) throw err;
         res.json({updated: poll})
     })
+}
+
+function checkVoted(id, user) {
+    return new Promise(function (resolve, reject) {
+        Poll.findById(id, function (err, data) {
+            if(err) throw err
+            var voted = data.voters
+            let ipIsNew = voted.every(function(hist){return hist !== user});
+            if(ipIsNew){
+                return resolve(true);
+            }else{
+                return resolve(false);
+            }
+        });
+    });
+
 }
 
 module.exports = router;
